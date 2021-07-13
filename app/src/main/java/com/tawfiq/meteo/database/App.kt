23161da517cb75ec7f939009e3f2ccd15edfc12e
PTMF02211 <ext.tawfiq.mouttalif@sncf.fr>
2021-07-13 17:01:52 +0200

    create database

---
+++ app/src/main/java/com/tawfiq/meteo/database/App.kt
@@ -0,0 +1,18 @@
+package com.tawfiq.meteo.database
+
+import android.app.Application
+
+class App : Application() {
+
+    companion object{
+        lateinit var instance: App
+
+        val database : DatabaseHelper by lazy {
+            DatabaseHelper(instance)
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/com/tawfiq/meteo/database/DatabaseHelper.kt
@@ -0,0 +1,42 @@
+package com.tawfiq.meteo.database
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
+import com.tawfiq.meteo.model.City
+
+
+private const val DATABASE_NAME= "meteo.db"
+private const val DATABASE_VERSION = 1
+
+private const val CITY_TABLE_NAME= "city"
+private const val CITY_COLUM_ID= "id"
+private const val CITY_COLUM_NAME= "name"
+
+private const val CITY_TABLE_CREATE=
+        """CREATE TABLE $CITY_TABLE_NAME ($CITY_COLUM_ID INTEGER PRIMARY KEY, $CITY_COLUM_NAME TEXT)"""
+
+class DatabaseHelper (context : Context) :
+        SQLiteOpenHelper(context,DATABASE_NAME,null, DATABASE_VERSION ){
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL(CITY_TABLE_CREATE)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        TODO("Not yet implemented")
+    }
+
+    fun createCity(city : City): Boolean{
+        val value = ContentValues()
+        value.put(CITY_COLUM_ID,city.name)
+        Log.i("city_create","value $value")
+
+        val id = writableDatabase.insert(CITY_TABLE_NAME,null,value)
+        city.id = id
+
+        return id > 0
+    }
+}
