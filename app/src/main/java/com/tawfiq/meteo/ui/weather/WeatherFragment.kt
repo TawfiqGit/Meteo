dba55e915c722e067497cddaf5bcd652c3c08efc
PTMF02211 <ext.tawfiq.mouttalif@sncf.fr>
2021-07-28 17:49:59 +0200

    show data of getWeather update strings.xml

--- app/src/main/java/com/tawfiq/meteo/ui/weather/WeatherFragment.kt
+++ app/src/main/java/com/tawfiq/meteo/ui/weather/WeatherFragment.kt
@@ -6,9 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import com.tawfiq.meteo.R
 import com.tawfiq.meteo.database.App
+import com.tawfiq.meteo.domain.Weather
 import com.tawfiq.meteo.domain.WeatherWrapper
 import com.tawfiq.meteo.ui.openweathermap.mapOpenWeatherDataToWeather
 import retrofit2.Call
@@ -18,6 +21,12 @@ import retrofit2.Response
 class WeatherFragment : Fragment() {
 
     private lateinit var cityName: String
+    private lateinit var textViewCity : TextView
+    private lateinit var textViewTemperature : TextView
+    private lateinit var textViewHumidity : TextView
+    private lateinit var textViewPressure : TextView
+    private lateinit var textViewDescription : TextView
+    private lateinit var imageViewIcon : ImageView
 
     companion object {
         const val EXTRA_WEATHER_CITY_NAME = "com.tawfiq.meteo.extra.EXTRA_WEATHER_CITY_NAME"
@@ -25,7 +34,14 @@ class WeatherFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_weather, container, false)
+        val v : View = inflater.inflate(R.layout.fragment_weather, container, false)
+        textViewCity = v.findViewById(R.id.textView_city)
+        textViewDescription = v.findViewById(R.id.textview_description)
+        textViewTemperature = v.findViewById(R.id.textViewTemperature)
+        textViewPressure = v.findViewById(R.id.textViewPressure)
+        textViewHumidity = v.findViewById(R.id.textViewHumidity)
+        imageViewIcon = v.findViewById(R.id.imageViewIcon)
+        return v
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -45,13 +61,23 @@ class WeatherFragment : Fragment() {
             override fun onResponse(call: Call<WeatherWrapper>?, response: Response<WeatherWrapper>?) {
                 response?.body()?.let {
                     val weather = mapOpenWeatherDataToWeather(it)// it contient copie de la propriété à l'intérieur de let
+                    updateUIWeather(weather,cityName)
                     Log.i("reponse","weather : $weather")
                 }
             }
 
             override fun onFailure(call: Call<WeatherWrapper>, t: Throwable) {
-                Toast.makeText(activity, "Could not load city weather $t", Toast.LENGTH_SHORT).show()
+                Toast.makeText(activity, "Please connect to internet $t", Toast.LENGTH_SHORT).show()
             }
         })
+    }
+
+    private fun updateUIWeather(weather: Weather, cityName : String){
+        textViewCity.text = cityName
+        textViewDescription.text = weather.description
+
+        textViewTemperature.text = getString(R.string.weather_temperature,weather.temperature.toInt())
+        textViewHumidity.text = getString(R.string.weather_humidity,weather.humidity)
+        textViewPressure.text = getString(R.string.weather_pressure,weather.pressure)
     }
 }
